5e704e86c6ed068c4f0376f9cbdab88acf5bca81
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-12-17 17:24:35 +0100

    Fix the :before size for document headings

--- src/locomotec.sitecontent/locomotec/sitecontent/subnavigation.py
+++ src/locomotec.sitecontent/locomotec/sitecontent/subnavigation.py
@@ -1,8 +1,11 @@
 from five import grok
 from Acquisition import aq_inner
 
+from Products.CMFCore.utils import getToolByName
+
 from plone.app.layout.viewlets.interfaces import IPortalFooter
 from Products.CMFCore.interfaces import IFolderish
+from Products.CMFDefault.interfaces import IDocument
 
 
 class SubnavigationViewlet(grok.Viewlet):
@@ -11,7 +14,14 @@ class SubnavigationViewlet(grok.Viewlet):
     grok.viewletmanager(IPortalFooter)
     grok.name('locomotec.sitecontent.SubnavigationViewlet')
 
+    def update(self):
+        self.has_items = len(self.contained_items()) > 0
+
     def contained_items(self):
         context = aq_inner(self.context)
-        items = context.getFolderContents()
-        return items
+        catalog = getToolByName(context, 'portal_catalog')
+        brains = catalog(object_provides=IDocument.__identifier__,
+                         path=dict(query='/'.join(context.getPhysicalPath()),
+                                   depth=1),
+                         review_state='published')
+        return brains
