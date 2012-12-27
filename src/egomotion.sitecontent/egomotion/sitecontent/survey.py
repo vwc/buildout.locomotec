f8c4f4ea56700ec30612ff003cff3d49cb957637
Christoph Boehner <cb@vorwaerts-werbung.de>
2012-12-27 20:59:26 +0100

    Add survey content type and survey tool
    - folderish survey content type for survey storeage
    - grok utility for session storage and survey data processing

---
+++ src/egomotion.sitecontent/egomotion/sitecontent/survey.py
@@ -0,0 +1,39 @@
+from five import grok
+from plone.directives import dexterity, form
+
+from zope import schema
+from zope.schema.interfaces import IContextSourceBinder
+from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+
+from zope.interface import invariant, Invalid
+
+from z3c.form import group, field
+
+from plone.namedfile.interfaces import IImageScaleTraversable
+from plone.namedfile.field import NamedImage, NamedFile
+from plone.namedfile.field import NamedBlobImage, NamedBlobFile
+
+from plone.app.textfield import RichText
+
+from z3c.relationfield.schema import RelationList, RelationChoice
+from plone.formwidget.contenttree import ObjPathSourceBinder
+
+from egomotion.sitecontent import MessageFactory as _
+
+
+# Interface class; used to define content-type schema.
+
+class ISurvey(form.Schema, IImageScaleTraversable):
+    """
+    A folderish survey holding participant objects
+    """
+
+
+class Survey(dexterity.Container):
+    grok.implements(ISurvey)
+
+
+class View(grok.View):
+    grok.context(ISurvey)
+    grok.require('zope2.View')
+    grok.name('view')
---
+++ src/egomotion.sitecontent/egomotion/sitecontent/surveytool.py
@@ -0,0 +1,80 @@
+from five import grok
+from zope.interface import Interface
+from zope.globalrequest import getRequest
+
+from collective.beaker.interfaces import ISession
+
+SESSION_KEY = 'Uh53dAfH2JPzI/lIhBvN72RJzZVv6zk5'
+
+
+class ISurveyTool(Interface):
+    """ Survey processing tool that stores data inside a beaker session
+        storage. The data is then saved as survey participation result
+    """
+
+    def get(context):
+        """ Get active survey session
+
+            @param uuid: optional catalog uuid of active participation
+        """
+
+    def destroy(context):
+        """ Destroy a survey session on submitting or by user
+            interaction
+        """
+
+    def add(context):
+        """ Add answers to a survey session or update existing data
+
+            @param uuid: catalog uuid of participation object
+            @param answers: serialized form data of survey answers
+        """
+
+    def update(context):
+        """ Update potentially autosaved form data in session
+            storage
+
+            @param uuid: catalog uuid of participation object
+            @param answers: serialized survey form data
+        """
+
+
+class SurveyTool(grok.GlobalUtility):
+    grok.provides(ISurveyTool)
+
+    def get(self, key=None):
+        survey_id = 'survey.%s' % SESSION_KEY
+        if key is not None:
+            survey_id = 'survey.%s' % key
+        session = ISession(getRequest())
+        if survey_id not in session:
+            session[survey_id] = dict()
+            session.save()
+        return session[survey_id]
+
+    def destroy(self, key=None):
+        survey_id = 'survey.%s' % SESSION_KEY
+        if key is not None:
+            survey_id = 'survey.%s' % key
+        session = ISession(getRequest())
+        if survey_id in session:
+            del session[survey_id]
+            session.save()
+
+    def add(self, uuid, answers=None):
+        """
+            Add item to survey session
+        """
+        survey = self.get()
+        item = self.update(uuid, answers)
+        if not item:
+            survey[uuid] = answers
+            return survey[uuid]
+
+    def update(self, uuid, answers):
+        survey = self.get()
+        item_id = uuid
+        if item_id in survey:
+            survey[item_id] = answers
+            return survey[item_id]
+        return None
