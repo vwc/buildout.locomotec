f9e7cf2b3087d5f067f8783a5bd709bff7e7be22
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-01-02 16:53:00 +0100

    Update to latest plone and several fixes

--- src/egomotion.sitecontent/egomotion/sitecontent/frontpage.py
+++ src/egomotion.sitecontent/egomotion/sitecontent/frontpage.py
@@ -2,8 +2,11 @@ from five import grok
 
 from plone.app.layout.navigation.interfaces import INavigationRoot
 
+from egomotion.sitecontent.interfaces import IEgomotionSite
+
 
 class FrontpageView(grok.View):
     grok.context(INavigationRoot)
+    grok.layer(IEgomotionSite)
     grok.require('zope2.View')
     grok.name('frontpage-view')
