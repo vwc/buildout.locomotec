b1959948e1a2ce6900d00572d11319727e2b1fdd
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-01-06 18:18:46 +0100

    Implement language specific views

--- src/locomotec.sitecontent/locomotec/sitecontent/eventbox.py
+++ src/locomotec.sitecontent/locomotec/sitecontent/eventbox.py
@@ -1,12 +1,13 @@
 from five import grok
 from Acquisition import aq_inner
+from plone import api
 
 from Products.CMFCore.utils import getToolByName
 
 from plone.app.layout.viewlets.interfaces import IPortalFooter
 from Products.CMFCore.interfaces import IFolderish
-from Products.ATContentTypes.interfaces.document import IATDocument
 
+from locomotec.sitecontent.newsfolder import INewsFolder
 from locomotec.sitecontent.newsentry import INewsEntry
 
 
@@ -17,12 +18,34 @@ class EventBoxViewlet(grok.Viewlet):
     grok.name('locomotec.sitecontent.EventBoxViewlet')
 
     def update(self):
+        self.has_newsfolder = len(self.get_newsfolders()) > 0
         self.has_items = len(self.get_items()) > 0
 
     def get_items(self):
         context = aq_inner(self.context)
+        if self.has_newsfolder:
+            archive = self.news_archive()
+            navroot = archive.getObject()
+        else:
+            navroot = api.portal.get_navigation_root(context)
         catalog = getToolByName(context, 'portal_catalog')
         brains = catalog(object_provides=INewsEntry.__identifier__,
+                         path=dict(query='/'.join(navroot.getPhysicalPath()),
+                                   depth=3),
                          review_state='published',
                          sort_on='start')
+        return brains
+
+    def news_archive(self):
+        folders = self.get_newsfolders()
+        return folders[0]
+
+    def get_newsfolders(self):
+        context = aq_inner(self.context)
+        navroot = api.portal.get_navigation_root(context)
+        catalog = getToolByName(context, 'portal_catalog')
+        brains = catalog(object_provides=INewsFolder.__identifier__,
+                         path=dict(query='/'.join(navroot.getPhysicalPath()),
+                                   depth=1),
+                         review_state='published')
         return brains
--- src/locomotec.sitecontent/locomotec/sitecontent/subnavigation.py
+++ src/locomotec.sitecontent/locomotec/sitecontent/subnavigation.py
@@ -1,11 +1,12 @@
 from five import grok
 from Acquisition import aq_inner
+from plone import api
+
+from zope.interface import Interface
 
 from Products.CMFCore.utils import getToolByName
 
 from plone.app.layout.viewlets.interfaces import IPortalFooter
-from Products.CMFCore.interfaces import IFolderish
-from Products.ATContentTypes.interfaces.document import IATDocument
 
 from locomotec.sitecontent.contentpage import IContentPage
 
@@ -27,3 +28,13 @@ class SubnavigationViewlet(grok.Viewlet):
                                    depth=1),
                          review_state='published')
         return brains
+
+
+class LanguageSwitch(grok.Viewlet):
+    grok.context(Interface)
+    grok.require('zope2.View')
+    grok.viewletmanager(IPortalFooter)
+    grok.name('locomotec.sitecontent.LanguageSwitch')
+
+    def switcher_base_url(self):
+        return api.portal.get().absolute_url()
