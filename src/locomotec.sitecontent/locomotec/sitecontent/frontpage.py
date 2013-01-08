ef000ff62a2a2f1c6f1ed1db93f39faf8b9a1253
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-01-08 15:56:38 +0100

    Automatically generated frontpage contents

--- src/locomotec.sitecontent/locomotec/sitecontent/frontpage.py
+++ src/locomotec.sitecontent/locomotec/sitecontent/frontpage.py
@@ -1,9 +1,29 @@
+from Acquisition import aq_inner
 from five import grok
+from plone import api
 
 from plone.app.layout.navigation.interfaces import INavigationRoot
+from plone.app.contentlisting.interfaces import IContentListing
+
+from locomotec.sitecontent.contentpage import IContentPage
 
 
 class FrontpageView(grok.View):
     grok.context(INavigationRoot)
     grok.require('zope2.View')
     grok.name('frontpage-view')
+
+    def update(self):
+        self.has_subpages = len(self.subpages()) > 0
+
+    def subpages(self):
+        context = aq_inner(self.context)
+        catalog = api.portal.get_tool(name='portal_catalog')
+        items = catalog(object_provides=IContentPage.__identifier__,
+                        path=dict(query='/'.join(context.getPhysicalPath()),
+                                  depth=1),
+                        review_state='published',
+                        sort_on='getObjPositionInParent',
+                        sort_limit=4)[:4]
+        results = IContentListing(items)
+        return results
