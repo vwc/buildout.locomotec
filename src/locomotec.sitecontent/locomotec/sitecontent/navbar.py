5f35efeedb30ffb3b3e042cabfa70884a774082b
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-01-08 17:47:13 +0100

    New english theme templates and navbar viewlet

---
+++ src/locomotec.sitecontent/locomotec/sitecontent/navbar.py
@@ -0,0 +1,54 @@
+from Acquisition import aq_inner
+from five import grok
+from plone import api
+
+from zope.interface import Interface
+
+from plone.app.layout.navigation.root import getNavigationRoot
+from plone.app.layout.navigation.navtree import NavtreeStrategyBase
+from plone.app.layout.navigation.navtree import buildFolderTree
+
+from plone.app.layout.viewlets.interfaces import IPortalFooter
+
+
+class NavbarViewlet(grok.Viewlet):
+    grok.context(Interface)
+    grok.require('zope2.View')
+    grok.viewletmanager(IPortalFooter)
+    grok.name('locomotec.sitecontent.NavbarViewlet')
+
+    def sections(self):
+        context = aq_inner(self.context)
+        portal = api.portal.get()
+        en_root = portal['en']
+        root = getNavigationRoot(context)
+        path = {'query': '/'.join(en_root.getPhysicalPath()),
+                'navtree': 2,
+                'navtree_start': 2}
+        query = {'path': path,
+                 'review_state': 'published',
+                 'portal_types': ('locomotec.sitecontent.contentpage',),
+                 'sort_on': 'getObjPositionInParent'}
+        root_obj = context.unrestrictedTraverse(root)
+        tree = buildFolderTree(root_obj, root_obj, query,
+                               strategy=NavtreeStrategyBase())
+        items = []
+        for c in tree['children']:
+            item = {}
+            item['item'] = c['item']
+            item['children'] = c.get('children', '')
+            items.append(item)
+        return items
+
+    def isActiveItem(self, itemid):
+        context = aq_inner(self.context)
+        context_id = context.getId()
+        if itemid == context_id:
+            return 'navitem active'
+        else:
+            return 'navitem'
+
+    def en_base_url(self):
+        portal_url = api.portal.get().absolute_url()
+        portal_url_en = portal_url + '/en'
+        return portal_url_en
